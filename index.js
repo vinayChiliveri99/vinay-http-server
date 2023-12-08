a6aa6796ec1cd4b1fb263f7fee9d9276c8b32fea
vinaychiliverii <vinaychiliveri99@gmail.com>
2023-12-08 12:31:15 +0530

    http-express added

--- index.js
+++ index.js
@@ -1,76 +1,80 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+
 const uuid = require('uuid');
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET' && req.url === '/html') {
-    res.setHeader('Content-Type', 'text/html');
-    const content = `
-        <!DOCTYPE html>
-        <html>
-          <head>
-          </head>
-          <body>
-              <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
-              <p> - Martin Fowler</p>
-        
-          </body>
-        </html>
-        `;
+let port = 3000;
 
-    res.end(content);
-  } else if (req.method === 'GET' && req.url === '/json') {
-    res.setHeader('Content-Type', 'json');
-    const jsonData = {
-      slideshow: {
-        author: 'Yours Truly',
-        date: 'date of publication',
-        slides: [
-          {
-            title: 'Wake up to WonderWidgets!',
-            type: 'all',
-          },
-          {
-            items: [
-              'Why <em>WonderWidgets</em> are great',
-              'Who <em>buys</em> WonderWidgets',
-            ],
-            title: 'Overview',
-            type: 'all',
-          },
-        ],
-        title: 'Sample Slide Show',
-      },
-    };
+// html data
 
-    res.end(JSON.stringify(jsonData));
-  } else if (req.method === 'GET' && req.url === '/uuid') {
-    let uuidData = uuid.v4();
-    const jsonObject = {
-      uuidData,
-    };
-    res.end(JSON.stringify(jsonObject));
-  } else if (req.method === 'GET' && req.url.startsWith('/status/')) {
-    let url = req.url.split('/');
-    let code = url.pop();
+app.get('/html', (req, res) => {
+  let htmlContent = `<!DOCTYPE html>
+  <html>
+    <head>
+    </head>
+    <body>
+        <h1>Any fool can write code that a computer can understand. Good programmers write code that humans can understand.</h1>
+        <p> - Martin Fowler</p>
+  
+    </body>
+  </html>`;
 
-    res.statusCode = code;
-
-    res.end(`a response with ${code}`);
-  } else if (req.method === 'GET' && req.url.startsWith('/delay/')) {
-    let url = req.url.split('/');
-    let seconds = url.pop();
-
-    setTimeout(() => {
-      res.end('200');
-    }, seconds * 1000);
-  } else {
-    res.statusCode = 404;
-    res.end('Not Found!!');
-  }
+  res.send(htmlContent);
 });
 
-let portNumber = 8080;
+// json data
+app.get('/json', (req, res) => {
+  let jsonData = {
+    slideshow: {
+      author: 'Yours Truly',
+      date: 'date of publication',
+      slides: [
+        {
+          title: 'Wake up to WonderWidgets!',
+          type: 'all',
+        },
+        {
+          items: [
+            'Why <em>WonderWidgets</em> are great',
+            'Who <em>buys</em> WonderWidgets',
+          ],
+          title: 'Overview',
+          type: 'all',
+        },
+      ],
+      title: 'Sample Slide Show',
+    },
+  };
 
-server.listen(portNumber, () =>
-  console.log(`Listening on ${portNumber}`)
-);
+  res.json(jsonData);
+});
+
+// uuid.v4
+
+app.get('/uuid', (req, res) => {
+  let uuidData = uuid.v4();
+  let obj = { uuidData };
+  res.json(obj);
+});
+
+// status/{status-code}
+
+app.get('/status/:code', (req, res) => {
+  let code = req.params.code;
+  res.status(code).send(`a response with code ${code}`);
+});
+
+// delay/{in-seconds}
+
+app.get('/delay/:seconds', (req, res) => {
+  let milli = req.params.seconds;
+  setTimeout(() => res.end('200'), parseInt(milli) * 1000);
+});
+
+// user specified error handler kind of..
+
+app.all('*', (req, res) => {
+  res.status(404).send(req.url + ' not found!!');
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
